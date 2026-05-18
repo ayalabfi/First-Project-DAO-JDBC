58e84e77df4378b3385404bfcf499983d0efb32d
Ayala Borba Filho <ayalaborbafilho@gmail.com>
2026-05-18 17:17:56 -0300

    findById implementation

--- src/model/dao/SellerDAO.java
+++ src/model/dao/SellerDAO.java
@@ -8,6 +8,6 @@ public interface SellerDAO {
     void insert(Seller obj);
     void update(Seller obj);
     void deleteById(Seller id);
-    Seller findById(Seller id);
+    Seller findById(Integer id);
     List<Seller> findAll();
 }
