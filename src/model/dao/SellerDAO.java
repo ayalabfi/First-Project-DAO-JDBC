e4b247c7fe3df6a03d2c61191ce723fd1900b44a
Ayala Borba Filho <ayalaborbafilho@gmail.com>
2026-05-18 16:55:48 -0300

    DepartmentDAO and SellerDAO interfaces

---
+++ src/model/dao/SellerDAO.java
@@ -0,0 +1,13 @@
+package model.dao;
+
+import model.entities.Seller;
+
+import java.util.List;
+
+public interface SellerDAO {
+    void insert(Seller obj);
+    void update(Seller obj);
+    void deleteById(Seller id);
+    Seller findById(Seller id);
+    List<Seller> findAll();
+}
