c8d11fc2363c4ecb7e7ac184630219476a385887
Ayala Borba Filho <ayalaborbafilho@gmail.com>
2026-05-19 09:42:08 -0300

    Delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,10 +7,13 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         SellerDAO sellerDAO = DAOFactory.createSellerDAO();
 
@@ -46,5 +49,13 @@ public class Program {
         seller.setName("Martha Waine");
         sellerDAO.update(seller);
         System.out.println("Update completed!");
+
+        System.out.println("\n=== Test 6: seller delete ===");
+        System.out.print("Enter id for delete test: ");
+        int id = sc.nextInt();
+        sellerDAO.deleteById(id);
+        System.out.println("Delete completed!");
+
+        sc.close();
     }
 }
